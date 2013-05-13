d7b4e1c4a06e7258b37316de2f7d59588e281dc7
Daniel Buckle <danielbuckle31@gmail.com>
2013-05-13 20:07:31 +0100

    Initial Commit

---
+++ FootyAppTester/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FootyAppTester
+//
+//  Created by Daniel Buckle on 13/05/2013.
+//  Copyright (c) 2013 Daniel Buckle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
